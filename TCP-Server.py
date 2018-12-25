36d25e9e7331db37ef1dc7ee579b67aab7e28d93
Nedal <44215261+CyberSy@users.noreply.github.com>
2018-12-25 16:47:59 +0200

    Update TCP-Server.py

--- TCP-Server.py
+++ TCP-Server.py
@@ -1,37 +1,39 @@
-import socket 
+import socket
 import threading
 
-bind_ip = "127.0.0.1" # Change This!
-bind_port = 4444	# Change This!
+bind_ip   = "127.0.0.1" #change this!
+bind_port = 9999 #change this! 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((bind_ip,bind_port))
 
-server.listen(1)
+server.listen(5)
 
-print "[*] listening on %s:%d" % (bind_ip,bind_port)
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
 
-# this is the client-handling thread 
-
+# this is our client handling thread
 def handle_client(client_socket):
 
-	# print out what the client sends
-	request = client_socket.recv(1024)
- 
-	print "[*] Received: %s" % request 
+    # just print out what the client sends
+    request = client_socket.recv(1024)
+    
+    print "[*] Received: %s" % request    
 
-	# send back a packet 
-	client_socket.send("Message Received! ")
+    # send back a packet
+    client_socket.send("ACK!")
+    print client_socket.getpeername()
+    client_socket.close()
 
-	client_socket.close()
 
 while True:
 
-	client,addr = server.accept()
+    client,addr = server.accept()
+    
+    print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
 
-	print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+    # spin up our client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
 
-	# spin up the client thread to handle incoming data 
-	client_handler = threading.Thread(target=handle_client,args=(client,))  
-	client_handler.start()
+
